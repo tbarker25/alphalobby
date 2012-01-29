bdd6a23e7760b98e8ed689ebb3d3c539bf1fd768
tb <tb@tb-PC.vic.bigpond.net.au>
2012-01-29 13:01:05 +1100

    Added outlines around starting boxes on minimap. NB: should seperate minimap image from starting boxes

--- src/rgba2c.c
+++ src/rgba2c.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <inttypes.h>
-
 #include <fcntl.h>
 
 int main(int argc, char **argv)
@@ -11,14 +10,9 @@ int main(int argc, char **argv)
 	uint8_t buff[16384];
 	size_t len;
 	while ((len = fread(buff, 1, sizeof(buff), stdin))) {
-		// uint32_t *buff32 = (void *)buff;
 		for (int i=0; i < len / 4; ++i)
 			fprintf(stdout, "0x%X, 0x%X, 0x%X, 0x%X, \n", 
 			buff[i*4+2], buff[i*4+1], buff[i*4], buff[i*4+3]);
-			// buff32[i]);
-					// (buff32[i] & 0x000000FF) << 16
-					// | (buff32[i] & 0x00FF0000) >> 16
-					// | (buff32[i] & 0xFF00FF00));
 	}
 	puts("};\n");
 	return 0;
